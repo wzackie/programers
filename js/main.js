62b40277e9c6c297bc7589c61bc3f0a25a306055
sondos mahmoud <wzackie.77@gmail.com>
2024-12-14 19:30:40 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+ function countdownTimer() {
+    const birthday = new Date("2025-12-12T00:00:00");
+    const now = new Date();
+    const difference = birthday - now;
+
+    if (difference > 0) {
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+    }
+ }
+ setInterval(countdownTimer, 1000);
